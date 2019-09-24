408331eaefa2553b3ca200bfb244b8bf2f735407
Convidado <pablohildo@gmail.com>
2019-09-24 19:11:28 -0300

    Adds part of lab5

--- Lab5-AlocacaoDinamica/vetorDinamico.c
+++ Lab5-AlocacaoDinamica/vetorDinamico.c
@@ -1,12 +1,15 @@
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "vetorDinamico.h"
 
 /* *********************************************** */
 /* *********************************************** */
 bool alocaVetor(tVetorDinamico* V, int n) {
-
-
-	return false;
+	V->V = (float*) malloc(n*sizeof(float));
+	V->MaxElem = n;
+	V->numElem = 0;
+	return V->V != NULL;
 }
 
 /* *********************************************** */
@@ -19,7 +22,14 @@ void desalocaVetor(tVetorDinamico* V) {
 /* *********************************************** */
 /* *********************************************** */
 bool preencheVetorAleatorio(tVetorDinamico* V, int n) {
-
+	if(n <= V->MaxElem){
+		for (int i = 0; i < n; i++)
+		{
+			V->V[i] = rand();
+		}
+		V->numElem = n;
+		return true;
+	}
 	return false;	
 }
 
@@ -42,7 +52,12 @@ void multiVetor(tVetorDinamico* V, float k) {
 /* *********************************************** */
 /* *********************************************** */
 bool copiaVetores(tVetorDinamico V0, tVetorDinamico* V1) {
-
+	if(V0.MaxElem <= V1->MaxElem){
+		for (int i = 0; i < V0.numElem; ++i)
+			*(V1->V)++ = *(V0.V)++;
+		V1->numElem = V0.numElem;
+		return true;
+	}
 	return false;
 	
 }
@@ -51,7 +66,12 @@ bool copiaVetores(tVetorDinamico V0, tVetorDinamico* V1) {
 /* *********************************************** */
 /* *********************************************** */
 bool somaVetores(tVetorDinamico V0, tVetorDinamico V1, tVetorDinamico* V2) {
-
+	if (V0.numElem == V1.numElem && V2->MaxElem >= V0.numElem){
+		for (int i = 0; i < V0.numElem; i++)
+			V2->V[i] = V0.V[i] + V1.V[i];
+		V2->numElem = V0.numElem;
+		return true;
+	}
 	return false;
 	
 }
